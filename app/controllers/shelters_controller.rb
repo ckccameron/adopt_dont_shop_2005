6e25077d7dea7bceea6779da1de2bcdaa2020a5f
Cam C <ckc.cameron@gmail.com>
2020-07-03 11:53:51 -0600

    Complete User Story 5

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -20,6 +20,12 @@ class SheltersController < ApplicationController
     @shelter = Shelter.find(params[:id])
   end
 
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update(shelter_params)
+    redirect_to "/shelters/#{shelter.id}"
+  end
+
   private
 
   def shelter_params
