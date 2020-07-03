04047b3f2b0540d7736cae5370bc7fb993010086
Cam C <ckc.cameron@gmail.com>
2020-07-03 10:47:56 -0600

    Add shelter seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shelter.create(name: "Dumb Friends League", address: "2080 S. Quebec St", city: "Denver", state: "CO", zip: "80231")
+
+Shelter.create(name: "MaxFund Animal Adoption Center", address: "1005 Galapago St", city: "Denver", state: "CO", zip: "80204")
+
+Shelter.create(name: "Colorado Puppy Rescue", address: "10021 E Iliff Ave", city: "Aurora", state: "CO", zip: "80247")
+
+Shelter.create(name: "PawsCo Headquarters", address: "8200 E Pacific Pl", city: "Denver", state: "CO", zip: "80231")
