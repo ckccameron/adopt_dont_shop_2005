210140d9b40e94d7bd357aff6612ecc2f8cb7cdd
Cam C <ckc.cameron@gmail.com>
2020-07-04 17:42:58 -0600

    Write spec for showing a pet

---
+++ spec/features/pets/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "pet show page" do
+  it "allows user to see a pet's image, name, description, approximate age, sex and adoption status" do
+    shelter_2 = Shelter.create(name: "MaxFund Animal Adoption Center")
+
+    pet_1 = Pet.create(image: "https://images.app.goo.gl/781WD1vckvbeUfNy5",
+                      name: "Chappelle",
+                      approximate_age: "2 years old",
+                      sex: "Male",
+                      shelter_id: shelter_2.id,
+                      description: "Pitbull",
+                      available_for_adoption: "Yes")
+
+    visit "/pets/#{pet_1.id}"
+
+    expect(page.find("img#pet-image-#{pet_1.id}"))
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_1.approximate_age)
+    expect(page).to have_content(pet_1.sex)
+    expect(page).to have_content("Shelter: #{shelter_2.name}")
+    expect(page).to have_content(pet_1.description)
+    expect(page).to have_content(pet_1.available_for_adoption)
+  end
+end
