0612abd1742cf7496b8edef671a0e5f1631ff55b
Cam C <ckc.cameron@gmail.com>
2020-07-04 21:09:10 -0600

    Add route for pets edit action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/pets', to: 'pets#index'
+  get '/shelters/:id/pets/new', to: 'pets#new'
+  post '/shelters/:id/pets', to: 'pets#create'
+  get '/pets/:id', to: 'pets#show'
+  get '/pets/:id/edit', to: 'pets#edit'
+
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
@@ -8,9 +14,6 @@ Rails.application.routes.draw do
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
   get '/shelters/:shelter_id/pets', to: 'shelters#pet_index'
-  get '/pets', to: 'pets#index'
-  get '/shelters/:id/pets/new', to: 'pets#new'
-  post '/shelters/:id/pets', to: 'pets#create'
-  get '/pets/:id', to: 'pets#show'
+
 
 end
