393fc618c5e581fd7d5d2ac1260313bcf8fcaa9e
Cam C <ckc.cameron@gmail.com>
2020-07-04 20:39:19 -0600

    Write new and create methods for pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,29 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @pet = Pet.new(shelter_id: params["shelter_id"])
+  end
+
+  def create
+    pet = Pet.new(
+      image: params["pet"]["image"],
+      name: params["pet"]["name"],
+      approximate_age: params["pet"]["approximate_age"],
+      sex: params["pet"]["sex"],
+      description: params["pet"]["description"],
+      shelter_id: params["shelter_id"],
+      adoptable: "Yes"
+      )
+    if pet.save
+      redirect_to "/shelters/#{shelter_id}/pets"
+    else
+      redirect_to "/shelters/#{shelter_id}/pets/new"
+    end
+  end
+
+  def shelter_id
+    params.permit(:shelter_id)[:shelter_id]
+  end
 end
