da81e9b671dc6418c9a7d731bd5d5ee6904ff0ca
Cam C <ckc.cameron@gmail.com>
2020-07-04 20:38:27 -0600

    Add routes for new and create actions for pets

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
-  get '/pets', to: 'pets#index'
   get '/shelters/:shelter_id/pets', to: 'shelters#pet_index'
+  get '/pets', to: 'pets#index'
+  get '/shelters/:id/pets/new', to: 'pets#new'
+  post '/shelters/:id/pets', to: 'pets#create'
   get '/pets/:id', to: 'pets#show'
+
 end
